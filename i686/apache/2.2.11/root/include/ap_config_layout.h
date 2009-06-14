006b977a5e8ce1e0e132c1b26f6541d16054c0e4
Matthew Iselin <Matthew Iselin@mattpc.(none)>
2009-06-14 16:51:17 +1000

    Adding Apache, OpenSSL, PHP and libncurses to the package manager.

---
+++ i686/apache/2.2.11/root/include/ap_config_layout.h
@@ -0,0 +1,64 @@
+/* Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * @file  ap_config_layout.h
+ * @brief Apache Config Layout
+ */
+
+#ifndef AP_CONFIG_LAYOUT_H
+#define AP_CONFIG_LAYOUT_H
+
+/* Configured Apache directory layout */
+#define DEFAULT_PREFIX ""
+#define DEFAULT_EXP_EXEC_PREFIX ""
+#define DEFAULT_REL_EXEC_PREFIX ""
+#define DEFAULT_EXP_BINDIR "/applications"
+#define DEFAULT_REL_BINDIR "/applications"
+#define DEFAULT_EXP_SBINDIR "/bin"
+#define DEFAULT_REL_SBINDIR "/bin"
+#define DEFAULT_EXP_LIBEXECDIR "/modules"
+#define DEFAULT_REL_LIBEXECDIR "/modules"
+#define DEFAULT_EXP_MANDIR "/man"
+#define DEFAULT_REL_MANDIR "/man"
+#define DEFAULT_EXP_SYSCONFDIR "/config/apache"
+#define DEFAULT_REL_SYSCONFDIR "/config/apache"
+#define DEFAULT_EXP_DATADIR "/support/apache"
+#define DEFAULT_REL_DATADIR "/support/apache"
+#define DEFAULT_EXP_INSTALLBUILDDIR "/support/apache/build"
+#define DEFAULT_REL_INSTALLBUILDDIR "/support/apache/build"
+#define DEFAULT_EXP_ERRORDIR "/support/apache/error"
+#define DEFAULT_REL_ERRORDIR "/support/apache/error"
+#define DEFAULT_EXP_ICONSDIR "/support/apache/icons"
+#define DEFAULT_REL_ICONSDIR "/support/apache/icons"
+#define DEFAULT_EXP_HTDOCSDIR "/support/apache/htdocs"
+#define DEFAULT_REL_HTDOCSDIR "/support/apache/htdocs"
+#define DEFAULT_EXP_MANUALDIR "/support/apache/manual"
+#define DEFAULT_REL_MANUALDIR "/support/apache/manual"
+#define DEFAULT_EXP_CGIDIR "/support/apache/cgi-bin"
+#define DEFAULT_REL_CGIDIR "/support/apache/cgi-bin"
+#define DEFAULT_EXP_INCLUDEDIR "/include"
+#define DEFAULT_REL_INCLUDEDIR "/include"
+#define DEFAULT_EXP_LOCALSTATEDIR "/support/apache"
+#define DEFAULT_REL_LOCALSTATEDIR "/support/apache"
+#define DEFAULT_EXP_RUNTIMEDIR "/support/apache/logs"
+#define DEFAULT_REL_RUNTIMEDIR "/support/apache/logs"
+#define DEFAULT_EXP_LOGFILEDIR "/support/apache/logs"
+#define DEFAULT_REL_LOGFILEDIR "/support/apache/logs"
+#define DEFAULT_EXP_PROXYCACHEDIR "/support/apache/proxy"
+#define DEFAULT_REL_PROXYCACHEDIR "/support/apache/proxy"
+
+#endif /* AP_CONFIG_LAYOUT_H */
