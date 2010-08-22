92b3ea533d1efee40f3a8cb70eca01b2970aea23
Matthew Iselin <matthew@theiselins.net>
2010-08-22 16:18:08 +1000

    Modify the buildPackages.sh to handle packages which provide headers/libraries that future packages require.

--- buildPackages.sh
+++ buildPackages.sh
@@ -1,12 +1,27 @@
 #!/bin/bash
 
-DIRS=$(find . -maxdepth 1 -type d -name "*")
-if [ -z $DIRS ]; then
-    for f in ./packages/*; do
-        echo
-        echo "---------- Building in directory $f ----------"
-        echo
-        ENVPATH=$PWD $f/build.sh
-    done
-fi
+BASEDIR=$PWD
+
+cd packages
+
+# Put any dependent libraries before the applications that need them
+DIRS="libgmp libmpfr curl newlib bash nasm gnumake binutils gcc prboom"
+
+for f in $DIRS; do
+    echo
+    echo "---------- Building in directory $f ----------"
+    echo
+    ENVPATH=$BASEDIR $f/build.sh
+    
+    # Packages that provide headers or libraries that other packages need are
+    # required to provide a "setupLinks.sh" script that automatically creates
+    # links in the cross-compiler for the package (as packages may have different
+    # layouts)
+    if [ -e $f/setupLinks.sh ]; then    
+        echo "    -> Linking to cross-compiler, future packages depend on '$f'"
+        ENVPATH=$BASEDIR ./$f/setupLinks.sh
+    fi
+done
+
+cd $BASEDIR
 
---
+++ packages/libgmp/setupLinks.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+package=gmp
+version=5.0.1
+
+if [ -z $ENVPATH ]; then
+    echo "ENVPATH not set, fixing" 1>&2
+    ENVPATH=../..
+fi
+
+source $ENVPATH/environment.sh
+
+BASE=$OUTPUT_BASE/$package/$version
+
+# Remove existing files to force our updated version/link
+[[ -e $CROSS_BASE/lib/libgmp.a ]] && rm $CROSS_BASE/lib/libgmp.a
+[[ -e $CROSS_BASE/include/gmp.h ]] && rm $CROSS_BASE/include/gmp.h
+
+ln -s $BASE/libraries/libgmp.a $CROSS_BASE/lib/libgmp.a
+
+# TODO: Find out why libgmp isn't using the --includedir directive!
+ln -s $BASE/support/gmp/include/gmp.h $CROSS_BASE/include/gmp.h
+
