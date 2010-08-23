048138cc27d4c765accf07753018644e497b8968
Matthew Iselin <matthew@theiselins.net>
2010-08-23 22:51:47 +1000

    dosbox: added build.sh.

--- environment.sh
+++ environment.sh
@@ -11,14 +11,20 @@ PACKMAN_PATH="$APPS_BASE/pup/pup"
 PACKMAN_REPO="$APPS_BASE/pup/package_repo"
 
 CC="$CROSS_BASE/bin/$ARCH_TARGET-pedigree-gcc"
-CXX="$CROSS_BASE/bin/$ARCH_TARGET-pedigree-g++"
+CXX="$CROSS_BASE/bin/$ARCH_TARGET-pedigree-gcc"
 CPP="$CROSS_BASE/bin/$ARCH_TARGET-pedigree-cpp"
 AS="$CROSS_BASE/bin/$ARCH_TARGET-pedigree-as"
 LD="$CROSS_BASE/bin/$ARCH_TARGET-pedigree-ld"
 LIBS="-lm"
 
-CFLAGS="-I$CROSS_BASE/include -I$CROSS_BASE/include/curl -I$CROSS_BASE/include/SDL -O3 -D__PEDIGREE__"
+CFLAGS="-I$CROSS_BASE/include/c++ -I$CROSS_BASE/include/c++/i686-pedigree"
+CFLAGS="$CFLAGS -I$CROSS_BASE/include -I$CROSS_BASE/include/curl -I$CROSS_BASE/include/SDL"
+CFLAGS="$CFLAGS -O3 -D__PEDIGREE__"
 CXXFLAGS=$CFLAGS
+
+# Include directories for the preprocessor
+CPPFLAGS="-I$CROSS_BASE/include/c++ -I$CROSS_BASE/include/c++/i686-pedigree"
+CPPFLAGS="$CPPFLAGS -I$CROSS_BASE/include -I$CROSS_BASE/include/curl -I$CROSS_BASE/include/SDL"
 
 LDFLAGS="-L$CROSS_BASE/lib"
 
---
+++ packages/dosbox/build.sh
@@ -0,0 +1,101 @@
+#!/bin/bash
+
+package=dosbox
+version=0.74
+url="http://sourceforge.net/projects/$package/files/$package/$version/$package-$version.tar.gz/download"
+
+echo "Building $package ($version)..."
+
+if [ -z $ENVPATH ]; then
+    echo "ENVPATH not set, fixing" 1>&2
+    ENVPATH=../..
+fi
+
+source $ENVPATH/environment.sh
+
+export CFLAGS
+export CXXFLAGS
+export LDFLAGS
+LIBS="-lSDL -lpedigree -lstdc++ $LIBS"
+export LIBS
+
+oldwd=$PWD
+
+mkdir -p $BUILD_BASE
+mkdir -p $DOWNLOAD_TEMP
+
+rm -rf $BUILD_BASE/build-$package-$version
+mkdir -p $BUILD_BASE/build-$package-$version
+cd $BUILD_BASE/build-$package-$version
+
+trap "rm -rf $BUILD_BASE/build-$package-$version; cd $oldwd; exit" INT TERM EXIT
+
+echo "    -> Grabbing source..."
+
+if [ ! -f $DOWNLOAD_TEMP/$package-$version.tar.gz ]; then
+    wget $url -nv -O $DOWNLOAD_TEMP/$package-$version.tar.gz
+fi
+
+cp $DOWNLOAD_TEMP/$package-$version.tar.gz .
+
+tar -xzf $package-$version.tar.gz --strip 1
+rm $package-$version.tar.gz
+
+echo "    -> Patching where necessary"
+
+patches=
+patchfiles=`find $SOURCE_BASE/$package/patches -maxdepth 1 -name "*.diff"`
+numpatches=`echo $patchfiles | wc -l`
+if [ ! -z "$patchfiles" ]; then
+    for f in $patchfiles; do
+        echo "       (applying $f)"
+        patch -p1 -d $BUILD_BASE/build-$package-$version/ < $f
+    done
+    
+    patches="#"
+fi
+if [ -e $SOURCE_BASE/$package/patches/$version/*.diff ]; then
+    for f in $SOURCE_BASE/$package/patches/$version/*.diff; do
+        echo "       (applying $version/$f)"
+        patch -p1 -d $BUILD_BASE/build-$package-$version/ < $f
+    done
+    
+    patches="#"
+fi
+
+if [ -N $patches ]; then
+    echo "       (no patches needed, hooray!)"
+fi
+
+mkdir -p $BUILD_BASE/build-$package-$version/build
+cd $BUILD_BASE/build-$package-$version/build
+
+set -e
+
+echo "    -> Configuring..."
+
+../configure --host=$ARCH_TARGET-pedigree --bindir=/applications \
+             --sysconfdir=/config/$package --datarootdir=/support/$package \
+             --prefix=/support/$package --disable-alsatest --disable-sdltest \
+             --disable-dynamic-core --disable-dynrec --disable-opengl \
+             2>&1 > /dev/null
+
+echo "    -> Building..."
+
+make $* 2>&1 > /dev/null
+
+echo "    -> Installing..."
+
+make DESTDIR="$OUTPUT_BASE/$package/$version/" install 2>&1 > /dev/null
+
+echo "Package $package ($version) has been built, now registering in the package manager"
+
+$PACKMAN_PATH makepkg --path $OUTPUT_BASE/$package/$version --repo $PACKMAN_REPO --name $package --ver $version
+$PACKMAN_PATH regpkg --repo $PACKMAN_REPO --name $package --ver $version
+
+cd $oldwd
+
+rm -rf $BUILD_BASE/build-$package-$version
+
+trap - INT TERM EXIT
+
