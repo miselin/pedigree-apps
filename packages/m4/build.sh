834a23403fd3bc5bae030cbcee281b615a1a41e0
Matthew Iselin <matthew@theiselins.net>
2010-08-28 21:14:42 +1000

    m4: build.sh

---
+++ packages/m4/build.sh
@@ -0,0 +1,97 @@
+#!/bin/bash
+
+package=m4
+version=1.4.14
+url="http://ftp.gnu.org/gnu/$package/$package-$version.tar.gz"
+
+echo "Building $package ($version)..."
+
+if [ -z $ENVPATH ]; then
+    echo "ENVPATH not set, fixing" 1>&2
+    ENVPATH=../..
+fi
+
+source $ENVPATH/environment.sh
+
+export CFLAGS
+export CXXFLAGS
+export LDFLAGS
+export LIBS
+
+oldwd=$PWD
+
+mkdir -p $BUILD_BASE
+mkdir -p $DOWNLOAD_TEMP
+
+rm -rf $BUILD_BASE/build-$package-$version
+mkdir -p $BUILD_BASE/build-$package-$version
+cd $BUILD_BASE/build-$package-$version
+
+trap "rm -rf $BUILD_BASE/build-$package-$version; cd $oldwd; exit" INT TERM EXIT
+
+echo "    -> Grabbing source..."
+
+if [ ! -f $DOWNLOAD_TEMP/$package-$version.tar.gz ]; then
+    wget $url -nv -O $DOWNLOAD_TEMP/$package-$version.tar.gz
+fi
+
+cp $DOWNLOAD_TEMP/$package-$version.tar.gz .
+
+tar -xzf $package-$version.tar.gz --strip 1
+rm $package-$version.tar.gz
+
+echo "    -> Patching where necessary"
+
+patches=
+if [ -e $SOURCE_BASE/$package/patches/*.diff ]; then
+    for f in $SOURCE_BASE/$package/patches/*.diff; do
+        echo "       (applying $f)"
+        patch -p1 -d $BUILD_BASE/build-$package-$version/ < $f
+    done
+    
+    patches="#"
+fi
+if [ -e $SOURCE_BASE/$package/patches/$version/*.diff ]; then
+    for f in $SOURCE_BASE/$package/patches/$version/*.diff; do
+        echo "       (applying $version/$f)"
+        patch -p1 -d $BUILD_BASE/build-$package-$version/ < $f
+    done
+    
+    patches="#"
+fi
+
+if [ -N $patches ]; then
+    echo "       (no patches needed, hooray!)"
+fi
+
+mkdir -p $BUILD_BASE/build-$package-$version/build
+cd $BUILD_BASE/build-$package-$version/build
+
+set -e
+
+echo "    -> Configuring..."
+
+../configure --host=$ARCH_TARGET-pedigree --bindir=/applications \
+             --sysconfdir=/config/$package --datarootdir=/support/$package \
+             --prefix=/support/$package \
+             2>&1 > /dev/null
+
+echo "    -> Building..."
+
+make $* 2>&1 > /dev/null
+
+echo "    -> Installing..."
+
+make DESTDIR="$OUTPUT_BASE/$package/$version/" install 2>&1 > /dev/null
+
+echo "Package $package ($version) has been built, now registering in the package manager"
+
+$PACKMAN_PATH makepkg --path $OUTPUT_BASE/$package/$version --repo $PACKMAN_REPO --name $package --ver $version
+$PACKMAN_PATH regpkg --repo $PACKMAN_REPO --name $package --ver $version
+
+cd $oldwd
+
+rm -rf $BUILD_BASE/build-$package-$version
+
+trap - INT TERM EXIT
+
