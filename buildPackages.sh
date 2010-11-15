1441ce2cbae283a0e285c55603619258165e876e
Matthew Iselin <matthew@theiselins.net>
2010-11-15 10:30:34 +1000

    Added lynx and lua to buildPackages.sh, and also added rudimentary build logging.

--- buildPackages.sh
+++ buildPackages.sh
@@ -6,13 +6,18 @@ cd packages
 
 # Put any dependent libraries before the applications that need them
 # TODO: sort.
-DIRS="zlib libiconv gettext libgmp libmpfr libmpc ncurses curl coreutils diffutils grep inetutils bsdtar e2fsprogs newlib bash m4 sed gzip nano nasm gnumake python binutils gcc dosbox prboom wget"
+DIRS="zlib libiconv gettext libgmp libmpfr libmpc ncurses curl coreutils diffutils grep inetutils bsdtar e2fsprogs newlib bash m4 sed gzip nano nasm gnumake python lynx lua binutils gcc dosbox prboom wget"
+
+echo > $BASEDIR/status.log
 
 for f in $DIRS; do
     echo
     echo "---------- Building in directory $f ----------"
     echo
     ENVPATH=$BASEDIR $f/build.sh
+
+    out="Failed."
+    [[ $? ]] && out="Success."
     
     # Packages that provide headers or libraries that other packages need are
     # required to provide a "setupLinks.sh" script that automatically creates
@@ -22,6 +27,11 @@ for f in $DIRS; do
         echo "    -> Linking to cross-compiler, future packages depend on '$f'"
         ENVPATH=$BASEDIR ./$f/setupLinks.sh
     fi
+
+    echo
+    echo "$f: $out"
+    echo "$f: $out" >> $BASEDIR/status.log
+    echo
 done
 
 cd $BASEDIR
