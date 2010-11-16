2e88d3d27448cd9bf8b04214c6e429cdd60759b9
Matthew Iselin <matthew@theiselins.net>
2010-11-16 15:43:39 +1000

    setupLinks.sh files for both libgmp and libiconv have been updated to link the created .so files.

--- packages/libgmp/setupLinks.sh
+++ packages/libgmp/setupLinks.sh
@@ -14,9 +14,11 @@ BASE=$OUTPUT_BASE/$package/$version
 
 # Remove existing files to force our updated version/link
 [[ -e $CROSS_BASE/lib/libgmp.a ]] && rm $CROSS_BASE/lib/libgmp.a
+[[ -e $CROSS_BASE/lib/libgmp.so ]] && rm $CROSS_BASE/lib/libgmp.so
 [[ -e $CROSS_BASE/include/gmp.h ]] && rm $CROSS_BASE/include/gmp.h
 
 ln -s $BASE/libraries/libgmp.a $CROSS_BASE/lib/libgmp.a
+ln -s $BASE/libraries/libgmp.so $CROSS_BASE/lib/libgmp.so
 
 # TODO: Find out why libgmp isn't using the --includedir directive!
 ln -s $BASE/support/gmp/include/gmp.h $CROSS_BASE/include/gmp.h
--- packages/libiconv/setupLinks.sh
+++ packages/libiconv/setupLinks.sh
@@ -15,12 +15,16 @@ BASE=$OUTPUT_BASE/$package/$version
 # Remove existing files to force our updated version/link
 [[ -e $CROSS_BASE/lib/libiconv.a ]] && rm $CROSS_BASE/lib/libiconv.a
 [[ -e $CROSS_BASE/lib/libcharset.a ]] && rm $CROSS_BASE/lib/libcharset.a
+[[ -e $CROSS_BASE/lib/libiconv.so ]] && rm $CROSS_BASE/lib/libiconv.so
+[[ -e $CROSS_BASE/lib/libcharset.so ]] && rm $CROSS_BASE/lib/libcharset.so
 [[ -e $CROSS_BASE/include/iconv.h ]] && rm $CROSS_BASE/include/iconv.h
 [[ -e $CROSS_BASE/include/libcharset.h ]] && rm $CROSS_BASE/include/libcharset.h
 [[ -e $CROSS_BASE/include/localcharset.h ]] && rm $CROSS_BASE/include/localcharset.h
 
 ln -s $BASE/libraries/libiconv.a $CROSS_BASE/lib/libiconv.a
 ln -s $BASE/libraries/libcharset.a $CROSS_BASE/lib/libcharset.a
+ln -s $BASE/libraries/libiconv.so $CROSS_BASE/lib/libiconv.so
+ln -s $BASE/libraries/libcharset.so $CROSS_BASE/lib/libcharset.so
 
 ln -s $BASE/include/iconv.h $CROSS_BASE/include/iconv.h
 ln -s $BASE/include/libcharset.h $CROSS_BASE/include/libcharset.h
