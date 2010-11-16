fee95ba8f047b0a38d496bf4ca92fc09d6867c12
Matthew Iselin <matthew@theiselins.net>
2010-11-17 09:50:40 +1000

    apache2: new scripts for new build system. Throws an error if building on OSX as the Apache configure script makes stupid choices when it sees Darwin (even when cross-compiling).

---
+++ packages/apache2/configure.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+source ./package-info.sh
+
+source "$1/environment.sh"
+(uname -s | grep Darwin > /dev/null 2>&1 ) && echo "Cross-compiling Apache is not yet possible on Darwin hosts. Skipping build."; exit 2
+
+export CFLAGS
+export CXXFLAGS
+export LDFLAGS
+LIBS="$LIBS -lpthread"
+export LIBS
+
+set -e
+
+cd "$2"
+mkdir -p build && cd build
+
+../configure --host=$ARCH_TARGET-pedigree --enable-layout=Pedigree \
+             --cache-file=$BUILD_BASE/build-$package-$version/pedigree.cache \
+             > /dev/null 2>&1
---
+++ packages/apache2/package-info.sh
@@ -0,0 +1,4 @@
+package=apache2
+urlpackage=httpd
+version=2.2.17
+url="http://apache.mirror.aussiehq.net.au/$urlpackage/$urlpackage-$version.tar.gz"
