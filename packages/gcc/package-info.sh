fc20411852e46a30a3e15e033b8ad519f8dd2393
Matthew Iselin <matthew@theiselins.net>
2010-11-17 17:40:58 +1000

    gcc: new scripts for new build system.

---
+++ packages/gcc/package-info.sh
@@ -0,0 +1,4 @@
+package=gcc
+version=4.5.1
+url="http://ftp.gnu.org/gnu/$package/$package-$version/$package-$version.tar.gz"
+
--- packages/gcc/setupLinks.sh
+++ packages/gcc/setupLinks.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
 
-package=gcc
-version=4.5.1
+source ./package-info.sh
 
-if [ -z $ENVPATH ]; then
-    echo "ENVPATH not set, fixing" 1>&2
-    ENVPATH=../..
-fi
-
-source $ENVPATH/environment.sh
+source "$1/environment.sh"
 
 BASE=$OUTPUT_BASE/$package/$version
 
