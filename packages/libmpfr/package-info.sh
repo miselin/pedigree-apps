4c40c6fd9495d232053406f90f6e05476931e0da
Matthew Iselin <matthew@theiselins.net>
2010-11-17 18:04:17 +1000

    libmpfr: new scripts for new build system.

---
+++ packages/libmpfr/package-info.sh
@@ -0,0 +1,5 @@
+package=libmpfr
+urlpackage=mpfr
+version=3.0.0
+url="http://www.mpfr.org/mpfr-current/$urlpackage-$version.tar.gz"
+
--- packages/libmpfr/setupLinks.sh
+++ packages/libmpfr/setupLinks.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
 
-package=mpfr
-version=3.0.0
+source ./package-info.sh
 
-if [ -z $ENVPATH ]; then
-    echo "ENVPATH not set, fixing" 1>&2
-    ENVPATH=../..
-fi
-
-source $ENVPATH/environment.sh
+source "$1/environment.sh"
 
 BASE=$OUTPUT_BASE/$package/$version
 
