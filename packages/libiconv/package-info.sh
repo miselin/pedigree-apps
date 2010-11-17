ea79aaf499eeac5fa5996667902b599e391b74bc
Matthew Iselin <matthew@theiselins.net>
2010-11-17 17:57:41 +1000

    libiconv: new scripts for new build system.

---
+++ packages/libiconv/package-info.sh
@@ -0,0 +1,4 @@
+package=libiconv
+version=1.13.1
+url="http://ftp.gnu.org/gnu/$package/$package-$version.tar.gz"
+
---
+++ packages/libiconv/pre-build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+source "$1/environment.sh"
+
+set -e
+
+cd "$2"
+
+aclocal -I m4 -I srcm4 > /dev/null 2>&1
+autoconf > /dev/null 2>&1
+
+cd preload
+aclocal -I ../m4 -I ../srcm4 > /dev/null 2>&1
+autoconf > /dev/null 2>&1
+
+cd ../libcharset
+cp ../m4/libtool.m4 ./m4/libtool.m4
+aclocal -I ../m4 -I ../srcm4
+autoconf > /dev/null 2>&1
+
