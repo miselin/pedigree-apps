9a91831a2ebdbc0ba9c1cfece83da8a6db327d95
Matthew Iselin <matthew@theiselins.net>
2013-03-23 11:08:44 +1000

    pedigree-posix-dev: updated mman.h to add msync

--- i686/pedigree-posix-dev/0.1/root/include/sys/mman.h
+++ i686/pedigree-posix-dev/0.1/root/include/sys/mman.h
@@ -13,11 +13,16 @@ _BEGIN_STD_C
 #define MAP_FIXED 4
 #define MAP_ANON 8
 
+#define MS_ASYNC 1
+#define MS_SYNC  2
+#define MS_INVALIDATE 4
+
 #define MAP_FAILED ((void*) 0)
 
 #include <sys/types.h>
 
 void  *mmap(void *, size_t, int, int, int, off_t);
+int    msync(void *addr, size_t len, int flags);
 int    munmap(void *, size_t);
 
 _END_STD_C
