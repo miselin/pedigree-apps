3a1dcf208b49e31b551c08211958b86236ecb2ca
Matthew Iselin <matthew@theiselins.net>
2013-04-02 15:39:10 +1000

    python: rename python -> python26 (to prepare for python 2.7 and 3.x)

--- packages/python/package-info.sh
+++
@@ -1,6 +0,0 @@
-package=python
-urlpackage=Python
-version=2.6.6
-shortversion=2.6
-url="http://python.org/ftp/$package/$version/$urlpackage-$version.tgz"
-
---
+++ packages/python26/package-info.sh
@@ -0,0 +1,7 @@
+package=python26
+urlname=python
+urlpackage=Python
+version=2.6.6
+shortversion=2.6
+url="http://python.org/ftp/$urlname/$version/$urlpackage-$version.tgz"
+
