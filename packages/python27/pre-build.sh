9845958726b0511c11685ddf241f7a0479711e3e
Matthew Iselin <matthew@theiselins.net>
2013-04-04 14:19:56 +1000

    python27: builds now (had to rejig all the patches). Yay, we have a modern Python!

--- packages/python27/pre-build.sh
+++ packages/python27/pre-build.sh
@@ -6,6 +6,9 @@ set -e
 
 cd "$2"
 set -e
+
+autoreconf > /dev/null 2>&1
+
 mkdir -p build && cd build
 
 ../configure > /dev/null 2>&1
@@ -15,21 +18,20 @@ is_darwin=`uname -s | grep -i darwin`
 set -e
 pyext=""
 if [ -z $is_darwin ]; then
-    make python Parser/pgen > /dev/null 2>&1
+    make Parser/pgen python > /dev/null 2>&1
 else
-    make python.exe Parser/pgen > /dev/null 2>&1
+    make Parser/pgen.exe python.exe > /dev/null 2>&1
     pyext=".exe"
 fi
 
 mv python$pyext ../hostpython
-mv Parser/pgen ../hostpgen
+mv Parser/pgen$pyext ../hostpgen
 
 rm -rf *
 
 mv ../hostpython ./
-mkdir Parser
+mkdir ./Parser
 mv ../hostpgen ./Parser/
 
 cd ..
-autoreconf > /dev/null 2>&1
 
