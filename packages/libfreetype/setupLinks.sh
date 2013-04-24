c265269c9941f22281e9a69d363455895cea644e
Matthew Iselin <matthew@theiselins.net>
2013-04-24 11:18:12 +1000

    libfreetype: setupLinks now does sane things

--- packages/libfreetype/setupLinks.sh
+++ packages/libfreetype/setupLinks.sh
@@ -6,14 +6,10 @@ source $1/environment.sh
 
 BASE=$OUTPUT_BASE/$package/$version
 
-echo "No links yet!"
-
-exit 0
-
 # Remove existing files to force our updated version/link
 [[ -e $CROSS_BASE/lib/libfreetype.a ]] && rm $CROSS_BASE/lib/libfreetype.a
 [[ -e $CROSS_BASE/include/ft2build.h ]] && rm $CROSS_BASE/include/ft2build.h
-[[ -e $CROSS_BASE/include/freetype2 ]] && rm -r $CROSS_BASE/include/freetype2
+[[ -e $CROSS_BASE/include/freetype ]] && rm -r $CROSS_BASE/include/freetype
 
 echo "        * libfreetype.a"
 ln -s $BASE/libraries/libfreetype.a $CROSS_BASE/lib/libfreetype.a
@@ -21,5 +17,5 @@ ln -s $BASE/libraries/libfreetype.a $CROSS_BASE/lib/libfreetype.a
 echo "        * ft2build.h"
 ln -s $BASE/include/ft2build.h $CROSS_BASE/include/ft2build.h
 echo "        * freetype2"
-ln -s $BASE/include/freetype2 $CROSS_BASE/include/freetype2
+ln -s $BASE/include/freetype2/freetype $CROSS_BASE/include/freetype
 
