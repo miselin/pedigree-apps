c465035f476735ce381d4682fdbf97276e7f10a0
Matthew Iselin <matthew@theiselins.net>
2013-05-04 21:45:09 +1000

    pedigree-base: add root bashrc and all of /config

--- packages/pedigree-base/special.sh
+++ packages/pedigree-base/special.sh
@@ -9,6 +9,7 @@ source $2/environment.sh
 mkdir pedigree-base
 mkdir pedigree-base/libraries
 mkdir pedigree-base/system
+mkdir pedigree-base/config
 
 # Copy files, show what we're doing
 echo "        -> libraries"
@@ -17,6 +18,10 @@ echo "        -> fonts"
 cp -R $PEDIGREE_BASE/images/base/system/fonts pedigree-base/system
 echo "        -> keymaps"
 cp -R $PEDIGREE_BASE/images/base/system/keymaps pedigree-base/keymaps
+echo "        -> config"
+cp -R $PEDIGREE_BASE/images/base/config pedigree-base/
+echo "        -> root .bashrc"
+cp $PEDIGREE_BASE/images/base/.bashrc pedigree-base/
 
 # Tar it up.
 tar -czf $1/$package-$version.tar.gz pedigree-base
