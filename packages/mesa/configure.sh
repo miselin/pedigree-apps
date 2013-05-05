d72ee97600c33c63513dfdef55b6769a2ec7e928
Matthew Iselin <matthew@theiselins.net>
2013-05-05 10:31:33 +1000

    mesa: --enable-shared, yay!

--- packages/mesa/configure.sh
+++ packages/mesa/configure.sh
@@ -13,14 +13,10 @@ set -e
 cd "$2"
 mkdir -p build && cd build
 
-# We disable shared for now as the mesa build system seems to assume
-# libtool versioning exists (even though it doesn't), and also because
-# $ARCH_TARGET-pedigree-gcc -shared apparently doesn't actually do the
-# right thing. Yay!
 ../configure --host=$ARCH_TARGET-pedigree --bindir=/applications \
              --sysconfdir=/config/$package --prefix=/support/$package \
              --libdir=/libraries --includedir=/include --disable-werror \
              --enable-osmesa --with-osmesa-bits=8 --disable-dri --disable-glx \
              --with-dri-drivers=swrast --without-gallium-drivers --disable-egl \
-             --disable-shared # > /dev/null 2>&1
+             --enable-shared > /dev/null 2>&1
 
