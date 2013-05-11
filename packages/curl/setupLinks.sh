5942f1fd4bf45d091781faae4203e45483584657
Matthew Iselin <matthew@theiselins.net>
2013-05-11 16:58:32 +1000

    curl: set up links to libcurl and header files after building

---
+++ packages/curl/setupLinks.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+source ./package-info.sh
+
+source "$1/environment.sh"
+
+BASE=$OUTPUT_BASE/$package/$version
+
+# Remove existing files to force our updated version/link
+[[ -e $CROSS_BASE/lib/libcurl.a ]] && rm $CROSS_BASE/lib/libcurl.a
+[[ -e $CROSS_BASE/include/curl ]] && rm $CROSS_BASE/include/curl
+
+ln -s $BASE/libraries/libcurl.a $CROSS_BASE/lib/libcurl.a
+
+ln -s $BASE/include/curl $CROSS_BASE/include/curl
+
