1961bb87c6163e954adae3003f53726c1f993bf8
Matthew Iselin <matthew@theiselins.net>
2013-05-21 20:19:56 +1000

    dropbear: new package dropbear, an ssh client (server and tools not yet built)

---
+++ packages/dropbear/configure.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+source ./package-info.sh
+
+source "$1/environment.sh"
+
+export CFLAGS
+export CXXFLAGS
+CPPFLAGS="$CPPFLAGS -DIP_TOS=0x1000 -Du_int8_t=uint8_t -Du_int16_t=uint16_t -Du_int32_t=uint32_t "
+export CPPFLAGS
+export LDFLAGS
+export LIBS
+
+set -e
+
+cd "$2"
+mkdir -p build && cd build
+
+../configure --host=$ARCH_TARGET-pedigree --bindir=/applications \
+             --sysconfdir=/config/$package --datarootdir=/support/$package \
+             --prefix=/support/$package --disable-lastlog # \
+             # > /dev/null 2>&1
+
---
+++ packages/dropbear/package-info.sh
@@ -0,0 +1,4 @@
+package=dropbear
+version=2013.58
+url="https://matt.ucc.asn.au/$package/releases/$package-$version.tar.bz2"
+bz2="yes"
