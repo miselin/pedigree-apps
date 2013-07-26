d2aaffd9f7adc0b6012182df850ce0492ee410ae
Matthew Iselin <matthew@theiselins.net>
2013-07-26 07:16:14 -0500

    netsurf: don't accept gzip encoding, and use Freetype for text rendering

--- packages/netsurf/build.sh
+++ packages/netsurf/build.sh
@@ -9,7 +9,7 @@ source "$1/environment.sh"
 CPPFLAGS="-I$2/prefix-framebuffer/include -DNO_IPV6=1 $CPPFLAGS"
 export CFLAGS="$CPPFLAGS $CFLAGS"
 export CXXFLAGS="$CPPFLAGS $CXXFLAGS"
-export LDFLAGS="$LDFLAGS -L$2/prefix-framebuffer/lib -Wl,--whole-archive -lnsfb -Wl,--no-whole-archive -lcss -ldom -lhubbub -lnsbmp -lnsgif -lparserutils -lrosprite -lsvgtiny -lwapcaplet -lcurl -liconv -lssl -lcrypto -lSDL -lpedigree -lstdc++ -lpthread"
+export LDFLAGS="$LDFLAGS -L$2/prefix-framebuffer/lib -Wl,--whole-archive -lnsfb -Wl,--no-whole-archive -lcss -ldom -lhubbub -lnsbmp -lnsgif -lparserutils -lrosprite -lsvgtiny -lwapcaplet -lcurl -liconv -lssl -lfreetype -lcrypto -lSDL -lpedigree -lstdc++ -lpthread"
 export LIBS
 
 set -e
@@ -29,7 +29,12 @@ sed -i.bak "s/^.*pkg_config_package.*sdl.*CFLAGS.*$//g" src/libnsfb*/Makefile
 # Change location of resources.
 sed -i.bak "s@share/netsurf/@support/netsurf@g" src/$package-$version/framebuffer/Makefile.defaults
 
+sed -i.bak "s|^.*SETOPT[(]CURLOPT\_ENCODING.*$|// \0|g" src/netsurf-3.0/content/fetchers/curl.c
+
+sed -i.bak "s/^.*CFLAGS.*freetype-config.*$/  CFLAGS += -DFB_USE_FREETYPE/g" src/netsurf-3.0/framebuffer/Makefile.target
+sed -i.bak "s/^.*LDFLAGS.*freetype-config.*$/  LDFLAGS += -lfreetype/g" src/netsurf-3.0/framebuffer/Makefile.target
+
 # prepareCompiler hooks in ccache to all compiling, don't double up.
 CC=$CC CXX=$CXX AR=$AR RANLIB=$RANLIB CCACHE="" \
-make TARGET=framebuffer HOST=pedigree VQ= Q= PKG_CONFIG="$CROSS_BASE/bin/$ARCH_TARGET-pedigree-pkg-config" NSFB_SDL_AVAILABLE="yes" NSFB_LINUX_AVAILABLE="no" NETSURF_USE_BMP="YES" NETSURF_USE_JPEG="NO" NETSURF_USE_MNG="NO" NETSURF_USE_GIF="YES" NETSURF_USE_LIBICONV_PLUG="NO" DESTDIR="$OUTPUT_BASE/$package/$version/" install
+make TARGET=framebuffer HOST=pedigree VQ= Q= PKG_CONFIG="$CROSS_BASE/bin/$ARCH_TARGET-pedigree-pkg-config" NSFB_SDL_AVAILABLE="yes" NSFB_LINUX_AVAILABLE="no" NETSURF_USE_BMP="YES" NETSURF_USE_JPEG="NO" NETSURF_USE_MNG="NO" NETSURF_USE_GIF="YES" NETSURF_FB_FONTLIB="freetype" NETSURF_USE_LIBICONV_PLUG="NO" DESTDIR="$OUTPUT_BASE/$package/$version/" install
 
