886b0ee2a33ee71b739f720a7917db8db1457503
Matthew Iselin <matthew@theiselins.net>
2013-10-19 06:50:17 -0500

    Updated DIRS for buildPackages to have some of our newer ports in it.

--- buildPackages.sh
+++ buildPackages.sh
@@ -13,7 +13,7 @@ DIRS="$DIRS libtool"
 
 # Put any dependent libraries before the applications that need them
 DIRS="$DIRS zlib libiconv gettext libgmp libmpfr libmpc ncurses curl expat pth"
-DIRS="$DIRS libpng libfreetype"
+DIRS="$DIRS libpng libfreetype fontconfig"
 
 # Cross-compilers for special libraries
 DIRS="$DIRS binutils gcc"
@@ -22,14 +22,20 @@ DIRS="$DIRS binutils gcc"
 DIRS="$DIRS bsdtar coreutils diffutils e2fsprogs grep gnumake gzip inetutils"
 DIRS="$DIRS m4 nano sed wget"
 
+# Secure
+DIRS="$DIRS openssl dropbear"
+
 # Games
 DIRS="$DIRS dosbox prboom"
 
 # Programming languages (non-GCC)
 DIRS="$DIRS lua nasm python26 python27"
 
+# Graphics stack
+DIRS="$DIRS cairo mesa"
+
 # Other applications
-DIRS="$DIRS apache2 bash lynx mtools"
+DIRS="$DIRS apache2 bash lynx mtools netsurf vim"
 
 # Apache modules
 DIRS="$DIRS "
