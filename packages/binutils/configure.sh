2027ec0cafc1d9ffec9d7319ecfd4bacd1450793
Matthew Iselin <matthew@theiselins.net>
2014-04-23 09:04:44 +1000

    binutils: upgrade to 2.24

--- packages/binutils/configure.sh
+++ packages/binutils/configure.sh
@@ -20,7 +20,10 @@ sed -i.bak -e 's/@colophon/@@colophon/' -e 's/doc@cygnus.com/doc@@cygnus.com/' b
 
 mkdir -p build && cd build
 
+# XXX: --disable-werror is needed because of an issue with rlimit().
+
 ../configure --host=$ARCH_TARGET-pedigree --target=$ARCH_TARGET-pedigree \
              --sysconfdir=/config/$package --prefix=/support/$package \
              --libdir=/libraries --includedir=/include --bindir=/applications \
+             --disable-werror \
              > /dev/null 2>&1
--- packages/binutils/package-info.sh
+++ packages/binutils/package-info.sh
@@ -1,3 +1,3 @@
 package=binutils
-version=2.22
+version=2.24
 url="http://ftp.gnu.org/gnu/$package/$package-$version.tar.gz"
