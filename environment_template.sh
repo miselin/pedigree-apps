063fe43517be5562993b0c147bb1135cffcd4a96
Matthew Iselin <matthew@theiselins.net>
2014-04-28 16:50:05 +1000

    misc: updated environment_template.sh to reflect pkg-config changes and x86_64 now being the default.

--- environment_template.sh
+++ environment_template.sh
@@ -1,6 +1,6 @@
 # Environment definitions - modify to suit your own environment
 
-ARCH_TARGET="i686"
+ARCH_TARGET="x86_64"
 PEDIGREE_BASE=""
 APPS_BASE=""
 CROSS_BASE="$PEDIGREE_BASE/pedigree-compiler"
@@ -15,20 +15,20 @@ CC="$CROSS_BASE/bin/$ARCH_TARGET-pedigree-gcc"
 CXX="$CROSS_BASE/bin/$ARCH_TARGET-pedigree-gcc"
 CPP="$CROSS_BASE/bin/$ARCH_TARGET-pedigree-cpp"
 AS="$CROSS_BASE/bin/$ARCH_TARGET-pedigree-as"
-LD="$CROSS_BASE/bin/$ARCH_TARGET-pedigree-ld"
+LD="$CROSS_BASE/bin/$ARCH_TARGET-pedigree-gcc"
 AR="$CROSS_BASE/bin/$ARCH_TARGET-pedigree-ar"
 RANLIB="$CROSS_BASE/bin/$ARCH_TARGET-pedigree-ranlib"
 LIBS="-lm"
 
-ARCH_BITS="32"
+ARCH_BITS="64"
 
-CFLAGS="-O3 -march=pentium4 -mtune=k8 -msse2 -D__PEDIGREE__"
+CFLAGS="-O3 -m$ARCH_BITS -march=k8 -msse2 -D__PEDIGREE__"
 CXXFLAGS=$CFLAGS
 
 # Include directories for the preprocessor
-CPPFLAGS="$CPPFLAGS -I$CROSS_BASE/include -I$CROSS_BASE/include/curl -I$CROSS_BASE/include/SDL -I$CROSS_BASE/include/ncurses -I$CROSS_BASE/include/pixman-1"
+CPPFLAGS="$CPPFLAGS -I$CROSS_BASE/include -I$CROSS_BASE/include/SDL -I$CROSS_BASE/include/ncurses"
 
-LDFLAGS="-L$CROSS_BASE/lib"
+LDFLAGS="-L$CROSS_BASE/lib -Wl,-rpath-link,$CROSS_BASE/lib"
 
 if [ -d "$CROSS_BASE/bin" ] && [[ ":$PATH:" != *":$CROSS_BASE/bin:"* ]]; then
     PATH="$CROSS_BASE/bin:$PATH"
