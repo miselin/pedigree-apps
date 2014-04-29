0a881127e235e14eabc65152a630d76e91065f31
Matthew Iselin <matthew@theiselins.net>
2014-04-29 12:43:36 +1000

    wget: version bump to 1.15

--- packages/wget/configure.sh
+++ packages/wget/configure.sh
@@ -7,7 +7,6 @@ source "$1/environment.sh"
 export CFLAGS
 export CXXFLAGS
 export LDFLAGS
-LIBS="$LIBS -lpthread"
 export LIBS
 
 set -e
@@ -18,5 +17,5 @@ mkdir -p build && cd build
 ../configure --host=$ARCH_TARGET-pedigree --bindir=/applications \
              --sysconfdir=/config/$package --datarootdir=/support/$package \
              --prefix=/support/$package --libdir=/libraries --includedir=/include \
-             > /dev/null 2>&1
+             --with-ssl=openssl > /dev/null 2>&1
 
--- packages/wget/package-info.sh
+++ packages/wget/package-info.sh
@@ -1,4 +1,4 @@
 package=wget
-version=1.12
+version=1.15
 url="http://ftp.gnu.org/gnu/$package/$package-$version.tar.gz"
 
