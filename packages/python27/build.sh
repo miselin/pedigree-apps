c1e34918b54fb72042f79458f2f8f53ad6648698
Matthew Iselin <matthew@theiselins.net>
2014-05-20 13:04:52 +1000

    python27: fixed build for 64-bit

--- packages/python27/build.sh
+++ packages/python27/build.sh
@@ -12,6 +12,6 @@ make HOSTPYTHON="$2/build/hostpython" HOSTPGEN="$2/build/Parser/hostpgen" python
 
 make HOSTPYTHON="$2/build/hostpython" \
      HOSTPGEN="$2/build/Parser/hostpgen" \
-     BLDSHARED="$ARCH_TARGET-pedigree-gcc -nostdlib -shared -Wl,-shared" \
+     BLDSHARED="$ARCH_TARGET-pedigree-gcc -shared -Wl,-shared" \
      CROSS_COMPILING=yes MACHDEP=pedigree ARCH_BITS="$ARCH_BITS" ARCH_TARGET="$ARCH_TARGET" \
      XCOMPILE_SYSINCLUDE="$CROSS_BASE/$ARCH_TARGET-pedigree/include" > /dev/null 2>&1
--- packages/python27/install.sh
+++ packages/python27/install.sh
@@ -10,8 +10,9 @@ cd "$2/build"
 
 make HOSTPYTHON=$BUILD_BASE/build-$package-$version/build/hostpython \
      HOSTPGEN=$BUILD_BASE/build-$package-$version/build/Parser/hostpgen \
-     BLDSHARED="$ARCH_TARGET-pedigree-gcc -nostdlib -shared -Wl,-shared" \
+     BLDSHARED="$ARCH_TARGET-pedigree-gcc -shared -Wl,-shared" \
      CROSS_COMPILING=yes MACHDEP=pedigree \
+     ARCH_BITS="$ARCH_BITS" ARCH_TARGET="$ARCH_TARGET" \
      DESTDIR=$OUTPUT_BASE/$package/$version install $* \
      > /dev/null 2>&1
 
