603bac2993111a74ebe8a40f06253dc9beb6a4aa
Matthew Iselin <matthew@theiselins.net>
2014-05-21 09:57:51 +1000

    ncurses: build shared libraries, libtinfo, and don't build C++ bindings yet (as the build system does strange, strange things)

--- packages/ncurses/configure.sh
+++ packages/ncurses/configure.sh
@@ -19,5 +19,7 @@ mkdir -p build && cd build
 ../configure --host=$ARCH_TARGET-pedigree --bindir=/applications \
              --sysconfdir=/config/$package --prefix=/support/$package \
              --libdir=/libraries --includedir=/include \
+             --with-libtool --with-shared --with-termlib \
+             --without-cxx-binding \
              > /dev/null 2>&1
 
--- packages/ncurses/setupLinks.sh
+++ packages/ncurses/setupLinks.sh
@@ -7,27 +7,20 @@ source "$1/environment.sh"
 BASE=$OUTPUT_BASE/$package/$version
 
 # Remove existing files to force our updated version/link
-[[ -e $CROSS_BASE/lib/libncurses.a ]] && rm $CROSS_BASE/lib/libncurses.a
-[[ -e $CROSS_BASE/lib/libncurses++.a ]] && rm $CROSS_BASE/lib/libncurses++.a
-[[ -e $CROSS_BASE/lib/libncurses_g.a ]] && rm $CROSS_BASE/lib/libncurses_g.a
-[[ -e $CROSS_BASE/lib/libform.a ]] && rm $CROSS_BASE/lib/libform.a
-[[ -e $CROSS_BASE/lib/libform_g.a ]] && rm $CROSS_BASE/lib/libform_g.a
-[[ -e $CROSS_BASE/lib/libmenu.a ]] && rm $CROSS_BASE/lib/libmenu.a
-[[ -e $CROSS_BASE/lib/libmenu_g.a ]] && rm $CROSS_BASE/lib/libmenu_g.a
-[[ -e $CROSS_BASE/lib/libpanel.a ]] && rm $CROSS_BASE/lib/libpanel.a
-[[ -e $CROSS_BASE/lib/libpanel_g.a ]] && rm $CROSS_BASE/lib/libpanel_g.a
+ln -sf $BASE/libraries/libncurses.a $CROSS_BASE/lib/
+ln -sf $BASE/libraries/libncurses.so $CROSS_BASE/lib/
+ln -sf $BASE/libraries/libncurses_g.a $CROSS_BASE/lib/
+ln -sf $BASE/libraries/libform.a $CROSS_BASE/lib/
+ln -sf $BASE/libraries/libform.so $CROSS_BASE/lib/
+ln -sf $BASE/libraries/libform_g.a $CROSS_BASE/lib/
+ln -sf $BASE/libraries/libmenu.a $CROSS_BASE/lib/
+ln -sf $BASE/libraries/libmenu.so $CROSS_BASE/lib/
+ln -sf $BASE/libraries/libmenu_g.a $CROSS_BASE/lib/
+ln -sf $BASE/libraries/libpanel.a $CROSS_BASE/lib/
+ln -sf $BASE/libraries/libpanel_g.a $CROSS_BASE/lib/
+ln -sf $BASE/libraries/libpanel.so $CROSS_BASE/lib/
+ln -sf $BASE/libraries/libtinfo.a $CROSS_BASE/lib/
+ln -sf $BASE/libraries/libtinfo.so $CROSS_BASE/lib/
 
-[[ -e $CROSS_BASE/include/ncurses ]] && rm -r $CROSS_BASE/include/ncurses
-
-ln -s $BASE/libraries/libncurses.a $CROSS_BASE/lib/libncurses.a
-ln -s $BASE/libraries/libncurses++.a $CROSS_BASE/lib/libncurses++.a
-ln -s $BASE/libraries/libncurses_g.a $CROSS_BASE/lib/libncurses_g.a
-ln -s $BASE/libraries/libform.a $CROSS_BASE/lib/libform.a
-ln -s $BASE/libraries/libform_g.a $CROSS_BASE/lib/libform_g.a
-ln -s $BASE/libraries/libmenu.a $CROSS_BASE/lib/libmenu.a
-ln -s $BASE/libraries/libmenu_g.a $CROSS_BASE/lib/libmenu_g.a
-ln -s $BASE/libraries/libpanel.a $CROSS_BASE/lib/libpanel.a
-ln -s $BASE/libraries/libpanel_g.a $CROSS_BASE/lib/libpanel_g.a
-
-ln -s $BASE/include $CROSS_BASE/include/ncurses
+ln -sf $BASE/include $CROSS_BASE/include/ncurses
 
