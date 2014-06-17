87c58fd499faf889be7ac0a98bdb8b7fd90679a1
Matthew Iselin <matthew@theiselins.net>
2014-06-17 21:34:25 +1000

    pedigree-devel: updated paths, no longer bombs /include with header files

--- packages/pedigree-devel/special.sh
+++ packages/pedigree-devel/special.sh
@@ -5,15 +5,17 @@
 source ./package-info.sh
 source $2/environment.sh
 
+gcc_vers=`$CROSS_BASE/bin/$ARCH_TARGET-pedigree-gcc -dumpversion`
+
 # Create directory tree.
 mkdir pedigree-devel
-mkdir -p pedigree-devel/libraries/gcc/$ARCH_TARGET-pedigree/4.5.1/
+mkdir -p pedigree-devel/libraries/gcc/$ARCH_TARGET-pedigree/$gcc_vers
 
 # Copy files, show what we're doing
 echo "        -> crt*.o"
-cp $PEDIGREE_BASE/build/kernel/crt*.o pedigree-devel/libraries/gcc/$ARCH_TARGET-pedigree/4.5.1/
-echo "        -> posix headers"
-cp -R $PEDIGREE_BASE/src/subsys/posix/include pedigree-devel/
+cp $PEDIGREE_BASE/build/kernel/crt*.o pedigree-devel/libraries/gcc/$ARCH_TARGET-pedigree/$gcc_vers/
+echo "        -> development headers"
+cp -R $PEDIGREE_BASE/src/subsys/posix/include pedigree-devel/support/gcc/include/
 
 # Tar it up.
 tar -czf $1/$package-$version.tar.gz pedigree-devel
