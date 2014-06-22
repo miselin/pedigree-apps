d25dcdf64473346ccf10c73157c6dbb35a0d9c90
Matthew Iselin <matthew@theiselins.net>
2014-06-23 08:46:58 +1000

    git: don't build git-gui, even if Tcl/Tk can be found on the build host.

--- packages/git/configure.sh
+++ packages/git/configure.sh
@@ -19,4 +19,4 @@ ac_cv_fread_reads_directories=no ac_cv_snprintf_returns_bogus=no \
 ./configure --host=$ARCH_TARGET-pedigree --bindir=/applications \
             --sysconfdir=/config/$package --datarootdir=/support/$package \
             --prefix=/support/$package --libdir=/libraries --includedir=/include \
-            --disable-ipv6
+            --disable-ipv6 --without-tcltk
