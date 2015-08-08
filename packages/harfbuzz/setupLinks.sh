b680eb3730c452073689d411aba8081decdf057a
Matthew Iselin <matthew@theiselins.net>
2015-08-08 16:53:46 +1000

    harfbuzz: build a shared library now

--- packages/harfbuzz/setupLinks.sh
+++ packages/harfbuzz/setupLinks.sh
@@ -8,9 +8,11 @@ BASE=$OUTPUT_BASE/$package/$version
 
 # Remove existing files to force our updated version/link
 [[ -e $CROSS_BASE/lib/libharfbuzz.a ]] && rm $CROSS_BASE/lib/libharfbuzz.a
+[[ -e $CROSS_BASE/lib/libharfbuzz.so ]] && rm $CROSS_BASE/lib/libharfbuzz.so
 [[ -e $CROSS_BASE/include/harfbuzz ]] && rm $CROSS_BASE/include/harfbuzz
 
 ln -s $BASE/libraries/libharfbuzz.a $CROSS_BASE/lib/libharfbuzz.a
+ln -s $BASE/libraries/libharfbuzz.so $CROSS_BASE/lib/libharfbuzz.so
 
 ln -s $BASE/include/harfbuzz $CROSS_BASE/include/harfbuzz
 
