d5e9281c662ea2d875f5c82b3436d0811de216dc
Matthew Iselin <matthew@theiselins.net>
2015-08-08 21:33:57 -0700

    glib: glib now builds

--- packages/glib/configure.sh
+++ packages/glib/configure.sh
@@ -7,15 +7,18 @@ source "$1/environment.sh"
 export CFLAGS
 export CPPFLAGS
 export LDFLAGS
+export LIBS="$LIBS -liconv"
 
 set -e
 
 cd "$2"
-mkdir -p build && cd build
+mkdir -p build-glib && cd build-glib
 
-glib_cv_stack_grows=no \
+glib_cv_stack_grows=no glib_cv_uscore=no ac_cv_func_posix_getpwuid_r=no \
+ac_cv_func_posix_getgrgid_r=no \
 ../configure --host=$ARCH_TARGET-pedigree --bindir=/applications \
              --sysconfdir=/config/$package --prefix=/support/$package \
              --libdir=/libraries --includedir=/include \
-             --with-sysroot=$CROSS_BASE --help
+             --with-sysroot=$CROSS_BASE --with-libiconv
 
+cp config.log $HOME/src/pedigree-apps/config.log
