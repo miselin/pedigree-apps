b0c18b3edc8b8745a27d26ea80fb697f0d21d2c9
Matthew Iselin <matthew@theiselins.net>
2015-08-09 04:31:03 -0700

    glib: fix setupLinks to not create links to everything in /libraries/

--- packages/glib/setupLinks.sh
+++ packages/glib/setupLinks.sh
@@ -6,6 +6,7 @@ source $1/environment.sh
 
 BASE=$OUTPUT_BASE/$package/$version
 
-ln -sf $BASE/libraries/* $CROSS_BASE/lib/
+ln -sf $BASE/libraries/*.so $CROSS_BASE/lib/
+ln -sf $BASE/libraries/*.a $CROSS_BASE/lib/
 ln -sf $BASE/include/* $CROSS_BASE/include/
 
