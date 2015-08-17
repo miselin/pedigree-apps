873df3eb62d7eaeda321a342de46372851ae6700
Matthew Iselin <matthew@theiselins.net>
2015-08-17 17:14:49 +1000

    runtests: dash is still annoying

--- runtests.sh
+++ runtests.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-if [ "x$COVERAGE" == "x" ]; then COVERAGE=coverage; fi
+if [ "x$COVERAGE" = "x" ]; then COVERAGE=coverage; fi
 
 $COVERAGE run --branch --source=. -m unittest discover -p '*_test.py'
 $COVERAGE html -d coverage_html
