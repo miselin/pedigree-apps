9a7fe1abd6f729b7a43ec2b2aba62a7486c0158d
Matthew Iselin <matthew@theiselins.net>
2015-08-17 04:06:57 -0700

    more python3 shenanigans

--- prepareChroot.py
+++ prepareChroot.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python2
+from __future__ import print_function
 
 import os
 import sys
@@ -10,15 +11,15 @@ from support import steps
 
 def main():
     if os.getuid():
-        print >>sys.stderr, 'prepareChroot must be run as root.'
+        print('prepareChroot must be run as root.', file=sys.stderr)
         exit(1)
 
     # Load up an environment ready for building.
     env = environment.generate_environment('')
 
-    print 'Creating chroot, please wait...'
+    print('Creating chroot, please wait...')
     steps.create_chroot(env)
-    print 'Chroot created!'
+    print('Chroot created!')
 
 
 if __name__ == '__main__':
