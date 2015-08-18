6ed9289b243c7fb874934c988c75c550fb699c60
Matthew Iselin <matthew@theiselins.net>
2015-08-18 02:53:14 -0700

    runtests: do a flake8 check of the tree after tests run

--- runtests.sh
+++ runtests.sh
@@ -4,3 +4,7 @@ if [ "x$COVERAGE" = "x" ]; then COVERAGE=coverage; fi
 
 $COVERAGE run --branch --source=. -m unittest discover -p '*_test.py'
 $COVERAGE html -d coverage_html
+
+pip install flake8
+
+flake8 --exclude=./packages/builds,./newpacks,./i686 .
