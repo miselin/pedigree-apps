f1661b936e5db575675a628bcd00284a810638a3
Matthew Iselin <matthew@theiselins.net>
2015-08-19 16:51:42 +1000

    runtests: install flake if not present (and inside a virtualenv)

--- runtests.sh
+++ runtests.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
+set -e
+
 if [ "x$COVERAGE" = "x" ]; then COVERAGE=coverage; fi
 
 $COVERAGE run --branch --source=. -m unittest discover -p '*_test.py'
 $COVERAGE html -d coverage_html
+
+if [ "x$VIRTUAL_ENV" != "x" ]; then pip install flake8; fi
 
 flake8 --exclude=./packages/builds,./newpacks,./i686,./venv,./downloads,./package-template,./packages,./pup/build,'*_test.py' .
