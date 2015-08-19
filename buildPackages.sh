6c2fc365641ae34512dcddfde30e19801c5ce34d
Matthew Iselin <matthew@theiselins.net>
2015-08-19 16:03:05 +1000

    build: make pup installs in buildPackages.sh quiet

--- buildPackages.sh
+++ buildPackages.sh
@@ -11,19 +11,17 @@ if [ "x$PATH_TO_CHROOT_SCRIPT" = "x" ]; then PATH_TO_CHROOT_SCRIPT=.; fi
 set -e
 
 # Install needed packages.
-pip install -r ./requirements.txt
-pip install pydot  # For generating dependencies.dot
+pip install -q -r ./requirements.txt
+pip install -q pydot  # For generating dependencies.dot
 
 # Make pup available for the builds.
-pip install --upgrade pup/
+pip install -q --upgrade pup/
 
 # Check the installation worked (will break the build if it did not).
 pup -h >/dev/null
 
-echo "Running $PATH_TO_CHROOT_SCRIPT/prepareChroot.py"
 sudo PYTHONPATH="$PWD:$PYTHONPATH" "$PATH_TO_CHROOT_SCRIPT/prepareChroot.py"
 
 target_arch="$1"
 shift
-echo "Now performing build proper."
 LD_PRELOAD=libfakechroot.so python ./buildPackages.py --target=$target_arch $*
