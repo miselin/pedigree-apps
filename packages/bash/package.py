99f329c3a0b3f60d4997cfe31c16522bec04fa64
Matthew Iselin <matthew@theiselins.net>
2015-08-27 13:59:17 +1000

    bash: fix bash build

--- packages/bash/package.py
+++ packages/bash/package.py
@@ -44,5 +44,5 @@ class BashPackage(buildsystem.Package):
         steps.make(srcdir, env)
 
     def deploy(self, env, srcdir, deploydir):
-        env['DESTDIR'] = deploydir
-        steps.make(srcdir, env, target='install')
+        steps.make(srcdir, env, target='install', extra_opts=(
+            'DESTDIR=%s' % deploydir,))
