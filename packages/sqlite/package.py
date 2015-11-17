d81b4525f42d8b6690e8f4009bfa66ca5d912fa4
Matthew Iselin <matthew@theiselins.net>
2015-11-17 15:11:49 +1100

    sqlite: new package sqlite, provides /applications/sqlite3

---
+++ packages/sqlite/package.py
@@ -0,0 +1,48 @@
+
+import os
+
+from support import buildsystem
+from support import steps
+
+
+class Sqlite(buildsystem.Package):
+
+    def __init__(self, *args, **kwargs):
+        super(Sqlite, self).__init__(*args, **kwargs)
+        self._options = buildsystem.Options()
+        self.tarfile_format = 'gz'
+
+    def name(self):
+        return 'sqlite'
+
+    def version(self):
+        return '3090200'
+
+    def build_requires(self):
+        return []
+
+    def patches(self, env, srcdir):
+        return []
+
+    def options(self):
+        return self._options
+
+    def download(self, env, target):
+        url = 'https://www.sqlite.org/2015/%(package)s-autoconf-%(version)s.tar.gz' % {
+            'package': self.name(),
+            'version': self.version(),
+        }
+        steps.download(url, target)
+
+    def prebuild(self, env, srcdir):
+        pass
+
+    def configure(self, env, srcdir):
+        steps.run_configure(self, srcdir, env)
+
+    def build(self, env, srcdir):
+        steps.make(srcdir, env)
+
+    def deploy(self, env, srcdir, deploydir):
+        env['DESTDIR'] = deploydir
+        steps.make(srcdir, env, target='install')
