48a13ab0b641519a920230db686ad5a941ad1677
Matthew Iselin <matthew@theiselins.net>
2016-04-19 10:48:00 +1000

    misc: ensure python3 compatibility for bump-build-worker.py

--- bump-build-worker.py
+++ bump-build-worker.py
@@ -1,5 +1,6 @@
 # -*- python -*-
 # ex: set syntax=python:
+from __future__ import print_function
 
 from oauth2client.client import GoogleCredentials
 from googleapiclient import discovery
@@ -17,18 +18,18 @@ def main():
 
     inst = get_instance()
     if inst and inst['status'] == 'RUNNING':
-        print 'Worker is running, all is OK.'
+        print('Worker is running, all is OK.')
         return
 
     # Start the worker.
-    print 'Starting worker...'
+    print('Starting worker...')
     result = compute.instances().start(zone=zone, project=project,
                                        instance='buildbot-slave').execute()
     if result.get('error'):
-        print 'Worker failed to start.'
+        print('Worker failed to start.')
         return
 
-    print 'Worker is starting...'
+    print('Worker is starting...')
 
 
 if __name__ == '__main__':
