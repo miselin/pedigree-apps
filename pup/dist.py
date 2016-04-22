e390114dd7592bedf507e3bd2d8c80dfe7633783
Matthew Iselin <matthew@theiselins.net>
2016-04-21 17:05:08 -0700

    pup: add very hacky script to upload new versions of pup

---
+++ pup/dist.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+import base64
+import sys
+import urllib
+
+
+def main():
+    key = sys.argv[1]
+    wheel_path = sys.argv[2]
+    version = sys.argv[3]
+
+    with open(wheel_path, 'rb') as f:
+        contents = f.read()
+
+    postdata = {
+        'version': version,
+        'blob': base64.b64encode(contents),
+        'key': 'upload',
+        'key_value': key,
+    }
+    postdata = urllib.urlencode(postdata)
+
+    url = 'https://the-pedigree-project.appspot.com/pup.whl'
+
+    response = urllib.urlopen(url, postdata)
+    result = response.read()
+
+    print result
+
+
+if __name__ == '__main__':
+    main()
