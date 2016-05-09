a2bc9b99e43778064f46a63f226b833e77745aa3
Matthew Iselin <matthew@theiselins.net>
2016-05-10 03:42:57 +1000

    build: mpc/gmp/mpfr dev packages needed for GCC bootstrap

--- install_deps_debian.sh
+++ install_deps_debian.sh
@@ -9,10 +9,12 @@ if [ $UID == 0 ]; then ELEVATE=; fi
 $ELEVATE apt-get install -y \
     build-essential libmpfr4 libgmp10 libmpc3 scons ccache diffutils patch \
     texinfo sqlite3 wget libsdl1.2-dev autoconf automake groff-base \
-    gtk-doc-tools python-libxml2 bison flex
+    gtk-doc-tools python-libxml2 bison flex libmpfr-dev libmpc-dev \
+    libgmp3-dev
 
 # TODO: libSDL-dev is just for sdl-config, which we should provide somewhere...
 
 # gtk-doc-tools is for pango et al (to make documentation)
 # python-libxml2 needed for mesa
 # flex, bison for grub2
+# libmpfr-dev, libmpc-dev, libgmp3-dev all for GCC's bootstrap
