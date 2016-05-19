5cc3cbdcd39abb852b94efa8e2d47b645c7ed772
Matthew Iselin <matthew@theiselins.net>
2016-05-19 15:08:14 +1000

    support: allow packages to have no download step

--- support/build.py
+++ support/build.py
@@ -167,8 +167,6 @@ def in_chroot(env, packages, package, package_id, download_filename):
                 os.unlink(download_target)
 
             raise
-    else:
-        raise Exception('download target %r does not exist!' % download_target)
 
     for step in pass1_steps:
         log.info('== %s %s step ==', package_id, step)
